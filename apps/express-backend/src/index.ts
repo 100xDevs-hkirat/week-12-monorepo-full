45dd4d5257eb61cbf1c0b8c650c3b90f6defdb0e
100xdevs <100xdevs@gmail.com>
2023-08-30 00:13:06 +0530

    test

--- apps/express-backend/src/index.ts
+++ apps/express-backend/src/index.ts
@@ -6,7 +6,7 @@ import { UserInput } from "common"
 app.get('/', (req: any, res: any) => {
   let parsedUser = UserInput.safeParse(req.body);
   if (!parsedUser.success) {
-    res.send('Incorrect input2!')
+    res.send('Incorrect input for harkirat!')
     return;
   }
   res.send('correct input!')
